63360679f91a476ef23bc123361cab371d042dfc
Gianpiero Addis <g.addis@lemundo.de>
2019-11-14 13:49:33 +0100

    Add docblock

--- lib/gitlab.rb
+++ lib/gitlab.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# The Gitlab client retrieves the open merge requests
+# for the groups specified in the configuration.
 class Gitlab
   def initialize(url, token, groups)
     @url = url

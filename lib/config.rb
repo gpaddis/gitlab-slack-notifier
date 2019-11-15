858ab311a99f17fd74ad4907c294b2ad78727b2b
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-11-15 09:54:25 +0100

    Implement error handling in Config

--- lib/config.rb
+++ lib/config.rb
@@ -10,14 +10,43 @@ class Config
               :slack_webhook_url
 
   def initialize(options = {})
-    @gitlab_url = options['gitlab_url']
-    @gitlab_token = options['gitlab_token']
-    @gitlab_group_ids = options['gitlab_group_ids']
-    @slack_webhook_url = options['slack_webhook_url']
+    self.gitlab_url = options['gitlab_url']
+    self.gitlab_token = options['gitlab_token']
+    self.gitlab_group_ids = options['gitlab_group_ids']
+    self.slack_webhook_url = options['slack_webhook_url']
+  end
+
+  def gitlab_url=(gitlab_url)
+    validate('gitlab_url', gitlab_url)
+    @gitlab_url = gitlab_url
+  end
+
+  def gitlab_token=(gitlab_token)
+    validate('gitlab_token', gitlab_token)
+    @gitlab_token = gitlab_token
+  end
+
+  def gitlab_group_ids=(gitlab_group_ids)
+    validate('gitlab_group_ids', gitlab_group_ids)
+    @gitlab_group_ids = gitlab_group_ids
+  end
+
+  def slack_webhook_url=(slack_webhook_url)
+    validate('slack_webhook_url', slack_webhook_url)
+    @slack_webhook_url = slack_webhook_url
+  end
+
+  # Raise a RuntimeError if the value is invalid.
+  def validate(key, value)
+    error = "Invalid value provided for #{key}. Check your configuration file."
+    raise error if value.nil? || value.empty?
   end
 
   # Create a new Config instance from a JSON file.
   def self.from_file(filename)
+    error = "Could not find #{filename} in the current directory."
+    raise error unless File.file?(filename)
+
     new(JSON.parse(File.read(filename)))
   end
 end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,13 +2,30 @@
 
 require 'config'
 
-RSpec.describe Config do
-  context 'with a configuration file' do
+RSpec.describe Config, '#from_file' do
+  context 'with a valid configuration file' do
     it 'reads the values from a file' do
       config_file = 'config.example.json'
       config = Config.from_file(config_file)
       expect(config.gitlab_url).to eq 'https://www.gitlab.com/'
       expect(config.gitlab_group_ids).to eq [3, 4, 5]
+    end
+  end
+
+  context 'without a valid configuration file' do
+    it 'raises an exception if the file does not exist' do
+      expect { Config.from_file('non-existing-file') }.to raise_error RuntimeError
+    end
+
+    it 'raises an exception if any of the options are empty' do
+      options = {
+        'gitlab_url' => 'https://www.gitlab.com/',
+        'gitlab_token' => '',
+        'gitlab_group_ids' => [1, 2, 3],
+        'slack_webhook_url' => ''
+      }
+      error_message = 'Invalid value provided for gitlab_token. Check your configuration file.'
+      expect { Config.new(options) }.to raise_error(error_message)
     end
   end
 end
